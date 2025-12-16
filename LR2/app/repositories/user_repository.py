fdbb598a01c659fa19755f267d42661bd7db294b
Daniil <89221449094dg@gmail.com>
2025-12-16 14:08:15 +0500

    LR3

---
+++ LR2/app/repositories/user_repository.py
@@ -0,0 +1,68 @@
+from typing import List
+from uuid import UUID
+
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from dto.user_dto import UserCreate, UserUpdate
+from entities import User
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(self, user_id: UUID) -> User | None:
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def get_by_filter(self, count: int, page: int, **kwargs) -> tuple[List[User], int]:
+        count_stmt = select(func.count(User.id))
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                count_stmt = count_stmt.where(getattr(User, key) == value)
+        total = (await self.session.execute(count_stmt)).scalar_one()
+
+        offset = count * page
+        stmt = select(User)
+
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                stmt = stmt.where(getattr(User, key) == value)
+
+        stmt = stmt.offset(offset).limit(count)
+        result = await self.session.execute(stmt)
+        return list(result.scalars().all()), total
+
+    async def create(self, user_data: UserCreate) -> User:
+        user = User(**user_data.model_dump())
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+
+    async def update(self, user_id: UUID, user_data: UserUpdate) -> User:
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise ValueError(f"Пользователь с ID {user_id} не найден")
+
+        update_data = user_data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(user, key, value)
+
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+
+    async def delete(self, user_id: UUID) -> None:
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()
+
+        if user:
+            await self.session.delete(user)
+            await self.session.commit()
